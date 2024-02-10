78346a15ac55bb4e80bcea76e90b89c78c5b0745
caiocroccia22 <caiocroccia22@gmail.com>
2024-02-10 15:55:06 -0300

    Hello World e rotas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Ola mundo!!!";
 });
+
+Route::get('/clientes', function () {
+    echo "Páginas com os clientes cadastrados";
+});
+
